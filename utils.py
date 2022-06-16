65932cfc945f7a370e270c5c0498ce4d2e52c302
carson <carson@cc-laptop-carson.attlocal.net>
2022-06-16 16:10:56 -0700

    huggingface

---
+++ utils.py
@@ -0,0 +1,15 @@
+
+
+def tokenize(element):
+    outputs = tokenizer(
+        element["content"],
+        truncation=True,
+        max_length=context_length,
+        return_overflowing_tokens=True,
+        return_length=True,
+    )
+    input_batch = []
+    for length, input_ids in zip(outputs["length"], outputs["input_ids"]):
+        if length == context_length:
+            input_batch.append(input_ids)
+    return {"input_ids": input_batch}
